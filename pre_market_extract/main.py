0e94e1f3d7b1db18875043a1b615f189b0cff45f
AZarbade <adwaitzarbade@gmail.com>
2024-05-10 12:39:24 +0530

    getting symbol list from NSE

--- pre_market_extract/main.py
+++ pre_market_extract/main.py
@@ -1,18 +1,20 @@
 import pandas as pd
-from nsepython import nse_eq, fnolist, nse_eq_symbols
+from nsepython import nse_eq, fnolist, logging
 
-print(fnolist())
+logging.basicConfig(level=logging.INFO)
 
-# symbols = pd.read_csv("pre_market_extract/symbol_lists/nifty_50.csv", header=None)[0]
-#
-# full_data = {}
-# for symbol in symbols:
-#     try:
-#         print(f"getting data for: {symbol}...")
-#         symbol_data = nse_eq(symbol)["preOpenMarket"]
-#         full_data[symbol] = symbol_data
-#     except Exception as e:
-#         print(f"Error fetching data for {symbol}: {e}")
-#
-# full_data = pd.DataFrame(full_data).transpose()
-# full_data.to_csv("pre_market_extract/full_data.csv")
+
+symbols = pd.DataFrame(fnolist())[0]
+
+full_data = {}
+for symbol in symbols:
+    try:
+        print(f"getting data for: {symbol}...")
+        symbol_data = nse_eq(symbol)["preOpenMarket"]
+        if symbol_data:
+            full_data[symbol] = symbol_data
+    except Exception as e:
+        print(f"Error fetching data for {symbol}: {e}")
+
+full_data = pd.DataFrame(full_data).transpose()
+full_data.to_csv("pre_market_extract/full_data.csv")
